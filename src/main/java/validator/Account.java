ab44825741fae4032f4bdec1cc7a4eb9541b27af
a.kondralev <a.kondralev@tinkoff.ru>
2021-12-29 09:36:52 +0300

    fix review

--- src/main/java/validator/Account.java
+++ src/main/java/validator/Account.java
@@ -3,7 +3,7 @@ package validator;
 public class Account {
     private String name;
     private final static int MIN_LENGTH = 3;
-    private final static int MAX_LENGTH = 20;
+    private final static int MAX_LENGTH = 19;
 
     public Account(String name) {
         this.name = name;
@@ -11,24 +11,19 @@ public class Account {
 
     public Boolean checkNameToEmboss(String name) {
         if (isSizeOk(name)) {
-            if (hasStrWhiteSpacesBeforeOrAfter(name)) {
-                if (hasStrOnlyOneWhiteSpace(name)) {
-                    return true;
-                }
-            }
+            return checkSpace(name);
         }
         return false;
     }
 
     private static boolean isSizeOk(String str) {
-        return str.length() >= MIN_LENGTH && str.length() < MAX_LENGTH;
+        if (str != null) {
+            return str.length() >= MIN_LENGTH && str.length() < MAX_LENGTH;
+        }
+        return false;
     }
 
-    private static boolean hasStrWhiteSpacesBeforeOrAfter(String str) {
-        return str.trim().length() == str.length();
-    }
-
-    private static boolean hasStrOnlyOneWhiteSpace(String str) {
-        return str.trim().replaceAll(" ", "").length() + 1 == str.trim().length();
+    private boolean checkSpace(String name) {
+        return name.matches("^[^\\s]+\\s[^\\s]+$");
     }
 }
--- src/test/java/ValidatorTest/AccountTest.java
+++ src/test/java/ValidatorTest/AccountTest.java
@@ -2,40 +2,38 @@ package ValidatorTest;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import validator.Account;
 
+import java.util.stream.Stream;
+
 public class AccountTest {
-    @ParameterizedTest
-    @ValueSource(
-            strings = {
-                    "BLACK",
-                    " GREY",
-                    "GREY ",
-                    " GREY ",
-                    "adfdfadfdfdf asdfsadfadfdasf",
-                    "ad",
-                    " GR EY ",
-                    ""
-            }
-    )
-    void checkNameToEmbossFalse(String name) {
-        Account account = new Account(name);
-        Assertions.assertFalse(account.checkNameToEmboss(name));
+
+    private static Stream<Arguments> testData() {
+        return Stream.of(
+                Arguments.of("B LACK", true),
+                Arguments.of("GRE Y", true),
+                Arguments.of("GR EY", true),
+                Arguments.of("G E", true),
+                Arguments.of("ddadfdadfdfdf asdf", true),
+                Arguments.of("BLACK", false),
+                Arguments.of(" GREY", false),
+                Arguments.of("GREY ", false),
+                Arguments.of(" GREY ", false),
+                Arguments.of("adfdfadfdf asdfsadf", false),
+                Arguments.of("adfdfadfdf asdfsadfk", false),
+                Arguments.of("ad", false),
+                Arguments.of(" GR EY ", false),
+                Arguments.of("GR  EY", false),
+                Arguments.of("", false),
+                Arguments.of(null, false));
     }
 
     @ParameterizedTest
-    @ValueSource(
-            strings = {
-                    "B LACK",
-                    "GRE Y",
-                    "GR EY",
-                    "G E",
-                    "ddadfdfadfdfdf asdf"
-            }
-    )
-    void checkNameToEmbossTrue(String name) {
+    @MethodSource("testData")
+    void checkNameToEm3boss(String name, boolean expected) {
         Account account = new Account(name);
-        Assertions.assertTrue(account.checkNameToEmboss(name));
+        Assertions.assertEquals(account.checkNameToEmboss(name), expected);
     }
 }
